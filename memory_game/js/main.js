8e966b0f6b8bb74156993b6b269ad5e06bb3d6ab
Sandhyarani <sandhyarani@192-168-1-193.tpgi.com.au>
2019-03-18 21:20:17 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,20 @@
-console.log("Up and Running");
-var cardOne = "queen1";
-var cardTwo = "queen2";
-var cardThree = "king1";
-var cardFour = "king2";
-console.log("User flipped"+ " " +cardTwo);
+console.log("Up and Running!");
+var cards = ["queen","queen","king","king"];
+var cardsInPlay= [];
+var cardOne = cards[0];
+cardsInPlay.push("cards[0]");
+console.log("user flipped queen");
+var cardTwo= cards[2];
+cardsInPlay.push("cards[2]");
+console.log("user flipped king");
+if(length.cardsInPlay === 2)
+{
+	alert("you found a match");
+}
+else if(cardsInPlay[0] === (cardsInPlay[2])
+{
+	alert("you found a match");
+}else if((length.cardsInPlay && (cardsInPlay[0] && cardsInPlay[2])) === 2)
+{
+	alert("sorry try again");
+}
